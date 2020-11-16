955a201cf9bce65e6e2fd09accfbfcace1b76f26
Kevin Forhan <kevin4forhan@gmail.com>
2020-11-16 13:36:38 -0600

    feat: added frontend

--- api/index.js
+++ api/index.js
@@ -9,3 +9,35 @@ const headers = {
     'Access-Control-Allow-Origin': '*', 
     'Access-Control-Allow-Methods': 'GET'
 }; 
+
+const decodeParams = searchParams => Array
+  .from(searchParams.keys())
+  .reduce((acc, key) => ({...acc, [key]: searchParams.get(key)}), {});
+
+  const server = createServer((req, res) => {
+      const requestUrl = url.parse(req.url)
+      const decodedParams = decodeParams(new URLSearchParams(requestUrl.search));
+      const { search, location, country = 'usa'} = decodedParams; 
+
+      const targetURL = `${config.BASE_URL}/${country.toLowerCase()}/${config.BASE_PARAMS}&app_id=${config.APP_ID}&app_key=${config.API_KEY}&what=${search}&where=${location}`;
+
+      if (req.method === 'GET') {
+          console.log(chalk.green(`Proxy GET request to : ${targetURL}`)); 
+          axios.get(targetURL)
+            .then(response => {
+                res.writeHead(200, headers)
+                res.end(JSON.stringify(response.data))
+            })
+            .catch(error => {
+                console.log(chalk.red(error))
+                res.writeHead(500, headers)
+                res.end(JSON.stringify(error))
+            }); 
+      }
+  })
+
+
+
+  server.listen(3001, () => {
+      console.log(chalk.green('Are you not entertained'))
+  })
---
+++ frontend/src/JobSearch.js
@@ -0,0 +1,7 @@
+class JobSearch {
+    constructor(
+        searchFormSelector, 
+        resultsContainerSelector, 
+        loadingElementSelector, 
+    )
+}
